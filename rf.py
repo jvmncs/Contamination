77e85a0782590029693a6b55118daf95c53744b8
jvmancuso <jmancuso17@jcu.edu>
2017-02-07 21:09:55 -0500

    remove custom grid search, add Bayes opt

--- rf.py
+++ rf.py
@@ -1,39 +1,32 @@
 # Spark ML Random Forest Classifier
-
-# Input data
-training=sqlContext.read.load("hdfs://**:**/labdata/trainfinal",format='parquet',header="false",inferSchema="true")
-validation = sqlContext.read.load("hdfs://**:**/labdata/valfinal",format="parquet",header="false",inferSchema="true")
-test = sqlContext.read.load("hdfs://**:**/labdata/testfinal",format="parquet",header="false",inferSchema="true")
-
 from pyspark.ml.classification import RandomForestClassifier
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
 import numpy as np
 
-def RFGenerator(nTreeList):
-	modelList=[]
-	if (type(nTreeList)==list) & (len(nTreeList)>0):
-		for x in nTreeList:
-			if type(x)==int:
-				modelList.append([RandomForestClassifier(numTrees=x),x])
-			else:
-				return "nTreeList must contain all integers."
-		return modelList
-	else:
-		return "nTreeList must be a nonempty list."
+# Input data
+training=sqlContext.read.load("hdfs://lebron:9000/labdata/trainfinal",format='parquet',header="false",inferSchema="true")
+validation = sqlContext.read.load("hdfs://lebron:9000/labdata/valfinal",format="parquet",header="false",inferSchema="true")
+test = sqlContext.read.load("hdfs://lebron:9000/labdata/testfinal",format="parquet",header="false",inferSchema="true")
 
-def RFAUCGather(modelList,traindf,valdf):
-	AUCList=[]
-	for x in modelList:
-		model = x[0].fit(traindf)
-		predsLabels = model.transform(valdf)
-		evaluator = BinaryClassificationEvaluator()
-		AUCList.append(evaluator.evaluate(predsLabels))
-	print "Max (Validation) AUC: "+str(max(AUCList))
-	print "Optimal number of trees: "+str(modelList[np.argmax(AUCList)][1])
-	return modelList[np.argmax(AUCList)][1]
+def measurableRF(numTrees=1, traindf=training, valdf=validation):
+	'''The function to be optimized with Bayesian Optimization, 
+	trained on traindf and evaluated on valdf.'''
+	RF = RandomForestClassifier(numTrees=round(numTrees), seed = 4321)
+	model = RF.fit(traindf)
+	predsLabels = model.transform(traindf)
+	evaluator = BinaryClassificationEvaluator()
+	print "Training AUC: "+str(evaluator.evaluate(predsLabels))
+	validationPL = model.transform(valdf)
+	evaluator = BinaryClassificationEvaluator()
+	return 	evaluator.evaluate(validationPL)
 
-def RFTrainer(numTrees,traindf):
-	RF = RandomForestClassifier(numTrees=numTrees)
+params={'numTrees':(5,1000)}
+RFBO = BayesianOptimization(measurableRF,params)
+RFBO.maximize(init_points=5,n_iter=50,acq='ei')
+
+def RFTrainer(numTrees=1,traindf=training):
+	'''Trains a random forest model on traindf with specified number of trees.'''
+	RF = RandomForestClassifier(numTrees=numTrees, seed=4321)
 	model = RF.fit(traindf)
 	predsLabels = model.transform(traindf)
 	evaluator = BinaryClassificationEvaluator()
@@ -42,13 +35,9 @@ def RFTrainer(numTrees,traindf):
 	print "Percent improvement from baseline accuracy: "+str(round(((predsLabels.filter(predsLabels.prediction==predsLabels.label).count()/float(predsLabels.count()))/(predsLabels.filter(predsLabels.label==0).count()/float(predsLabels.count()))-1)*100,5))+"%."
 	return model
 
-def RFModel(nTreeList):
-	modelList = RFGenerator(nTreeList)
-	numTrees = RFAUCGather(modelList,validation)
-	return RFTrainer(numTrees,training)
-
-def modelSummary(model):
-	testPL = model.transform(test)
+def modelSummary(model,testdf=test):
+	'''Prints out test metrics for model as evaluated on testdf.'''
+	testPL = model.transform(testdf)
 	evaluator = BinaryClassificationEvaluator(rawPredictionCol="prediction")
 	testAUC = evaluator.evaluate(testPL)
 	testAcc = testPL.filter(testPL.prediction==testPL.label).count()/float(testPL.count())
@@ -60,4 +49,15 @@ def modelSummary(model):
 	print "Percent improvement from test baseline accuracy: {0}%.".format(testPercImp)
 	print "Test Sensitivity: {0}.".format(testSens)
 	print "Test Specificity: {0}.".format(testSpec)
-	return
+	pass
+
+#Max Validation AUC: 0.99706426161945494
+#>>> modelSummary(RFTrainer(144))
+#Training AUC: 0.998993859919
+#Training Accuracy: 0.997452652564
+#Percent improvement from baseline accuracy: 1.13419%.
+#Test AUC: 0.931876671039.
+#Test Accuracy: 0.997076023392.
+#Percent improvement from test baseline accuracy: 1.07447%.
+#Test Sensitivity: 0.86486.
+#Test Specificity: 0.99889.
