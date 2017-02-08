87dad968b250214f5db23d29d1fd2a9f063a5c00
jvmancuso <jmancuso17@jcu.edu>
2017-02-07 21:07:37 -0500

    remove custom grid search, add bayes opt

--- gradientboostedtrees.py
+++ gradientboostedtrees.py
@@ -1,49 +1,45 @@
 # Spark ML Gradient-Boosted Trees
-
-# Input data
-training=sqlContext.read.load("hdfs://**:**/labdata/trainfinal",format='parquet',header="false",inferSchema="true")
-validation = sqlContext.read.load("hdfs://**:**/labdata/valfinal",format="parquet",header="false",inferSchema="true")
-test = sqlContext.read.load("hdfs://**:**/labdata/testfinal",format="parquet",header="false",inferSchema="true")
-
 from pyspark.ml.classification import GBTClassifier
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
 import numpy as np
 
-def gridBuildGBT(maxDepthList,maxBinsList,maxIterList,stepSizeList):
-	gridList = []
-	for x in maxDepthList:
-		for y in maxBinsList:
-			for z in maxIterList:
-				for a in stepSizeList:
-					paramDict = {"maxDepth":x,"maxBins":y, "maxIter":z, "stepSize":a}
-					gridList.append(paramDict)
-	return gridList
-
-def GBTGenerator(gridList):
-	modelList = []
-	for x in gridList:
-		modelList.append([GBTClassifier(maxDepth = x["maxDepth"], maxBins = x["maxBins"], maxIter = x["maxIter"], stepSize = x['stepSize'], seed = 4321),x])
-	return modelList
-
-def gridSearchGBT(modelList, traindf,valdf):
-	AUCList=[]
-	for x in modelList:
-		model = x[0].fit(traindf)
-		predsLabels = model.transform(valdf)
-		evaluator = BinaryClassificationEvaluator(rawPredictionCol="prediction")
-		AUCList.append(evaluator.evaluate(predsLabels))
-	print "Max (Validation) AUC: "+str(max(AUCList))+".\nOptimal maxDepth: "+str(modelList[np.argmax(AUCList)][1]["maxDepth"])+".\nOptimal maxBins: "+str(modelList[np.argmax(AUCList)][1]["maxBins"])+".\nOptimal maxIter: "+str(modelList[np.argmax(AUCList)][1]["maxIter"])+".\nOptimal stepSize: "+str(modelList[np.argmax(AUCList)][1]["stepSize"])
-	return [modelList[np.argmax(AUCList)][1]["maxDepth"], modelList[np.argmax(AUCList)][1]["maxBins"], modelList[np.argmax(AUCList)][1]["maxIter"], modelList[np.argmax(AUCList)][1]["stepSize"]]
+# Input data
+training=sqlContext.read.load("hdfs://lebron:9000/labdata/trainfinal",format='parquet',header="false",inferSchema="true")
+validation = sqlContext.read.load("hdfs://lebron:9000/labdata/valfinal",format="parquet",header="false",inferSchema="true")
+test = sqlContext.read.load("hdfs://lebron:9000/labdata/testfinal",format="parquet",header="false",inferSchema="true")
 
 # Final paramList = [6,40,25,.1]
 
-def GBTTrainer(paramList,traindf):
-	# paramList = [maxDepth, maxBins, maxIter, stepSize]
-	model = GBTClassifier(maxDepth = paramList[0], maxBins = paramList[1], maxIter = paramList[2], stepSize = paramList[3], seed = 4321)
-	return model.fit(traindf)
+def measurableGBT(maxBins=256, maxDepth=64, stepSize=.01, subsamplingRate=.8, maxiter=500, traindf=training, valdf=validation):
+	'''The function to be optimized with Bayesian Optimization, 
+	trained on traindf and evaluated on valdf.'''
+	GBT = GBTClassifier(maxDepth=round(maxDepth), maxBins = round(maxBins), maxIter=round(maxIter), stepSize=stepSize, subsamplingRate=subsamplingRate)
+	model = GBT.fit(traindf)
+	predsLabels = model.transform(traindf)
+	evaluator = BinaryClassificationEvaluator()
+	print "Training AUC: "+str(evaluator.evaluate(predsLabels))
+	validationPL = model.transform(valdf)
+	evaluator = BinaryClassificationEvaluator()
+	return 	evaluator.evaluate(validationPL)
 
-def modelSummary(model):
-	testPL = model.transform(test)
+params={'stepSize':(.00001,.3),'subsamplingRate':(.6,1)
+GBTBO = BayesianOptimization(measurableGBT,params)
+GBTBO.maximize(init_points=5,n_iter=50, acq='ei')
+
+def GBTTrainer(maxBins=256, maxDepth=64, stepSize=.01, subsamplingRate=.8, maxiter=500, traindf=training):
+	'''Trains a gradient boosted trees model on traindf with specified parameters.'''
+	GBT = GBTClassifier(maxDepth=round(maxDepth), maxBins=round(maxBins), maxIter=round(maxIter), stepSize=stepSize, subsamplingRate=subsamplingRate, seed=4321)
+	model = GBT.fit(traindf)
+	predsLabels = model.transform(traindf)
+	evaluator = BinaryClassificationEvaluator()
+	print "Training AUC: "+str(evaluator.evaluate(predsLabels))
+	print "Training Accuracy: "+str(predsLabels.filter(predsLabels.prediction==predsLabels.label).count()/float(predsLabels.count()))
+	print "Percent improvement from baseline accuracy: "+str(round(((predsLabels.filter(predsLabels.prediction==predsLabels.label).count()/float(predsLabels.count()))/(predsLabels.filter(predsLabels.label==0).count()/float(predsLabels.count()))-1)*100,5))+"%."
+	return model
+
+def modelSummary(model, testdf=test):
+	'''Prints out test metrics for model as evaluated on testdf.'''
+	testPL = model.transform(testdf)
 	evaluator = BinaryClassificationEvaluator(rawPredictionCol="prediction")
 	testAUC = evaluator.evaluate(testPL)
 	testAcc = testPL.filter(testPL.prediction==testPL.label).count()/float(testPL.count())
@@ -55,10 +51,4 @@ def modelSummary(model):
 	print "Percent improvement from test baseline accuracy: {0}%.".format(testPercImp)
 	print "Test Sensitivity: {0}.".format(testSens)
 	print "Test Specificity: {0}.".format(testSpec)
-	return
-
-# Test AUC: 0.91725163474.
-# Test Accuracy: 0.99451754386.
-# Percent improvement from test baseline accuracy: 0.81512%.
-# Test Sensitivity: 0.83784.
-# Test Specificity: 0.99667.
+	pass
